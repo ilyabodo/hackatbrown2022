bd39767ddf821556e34b753278f37182df078211
Sebastian Martino <sbfmartino@gmail.com>
2022-01-29 19:58:07 -0500

    Restructured Code as fsm

--- oled_temp_hum_1.ino/oled_temp_hum_1.ino.ino
+++ oled_temp_hum_1.ino/oled_temp_hum_1.ino.ino
@@ -13,8 +13,29 @@ const int yellowHum = 5;
 
 const int *allPins[10] = {&r_1, &g_1, &b_1, &r_2, &g_2, &b_2, &redTemp, &blueTemp, &greenHum, &yellowHum};
 
+// Define state enums
+typedef enum {
+  sProgram = 1,
+  sGetData = 2,
+  sSetOutput = 3,
+} state;
+
+float curr_temp;
+float curr_humid;
+
+int max_temp;
+int min_temp;
+int max_humid;
+int min_humid;
+
+// Set starting state to 'Program'
+state curr_state = sProgram;
+
 void setup() {
   Serial.begin(9600);
+  // One minute read timeout
+  Serial.setTimeout(60000);
+  while(!Serial);
   Environment.begin();
   Oled.begin();
   Oled.setFlipMode(false);
@@ -25,57 +46,81 @@ void setup() {
 }
 
 void loop() {
+  update_fsm();
+}
 
+void update_fsm() {
+  switch (curr_state) {
+    case sProgram:
+      Serial.println("Input maximum temperature (Celcius)");
+      max_temp = Serial.parseInt();
   
-  float currTemp = Environment.readTemperature();
-  float currHum = Environment.readHumidity();
-  digitalWrite(r_1, LOW);
-  digitalWrite(g_1, HIGH);
-  digitalWrite(b_1, LOW);
-  if (currTemp > 15.0){
-    digitalWrite(redTemp, HIGH);
-  }
-  else{
-    digitalWrite(redTemp, LOW);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-  }
-  if (currTemp < 10.0){
-    digitalWrite(blueTemp, HIGH);
-  }
-  else{
-    digitalWrite(blueTemp, LOW);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-  }
-
-
-  if (currHum > 70){
-    digitalWrite(greenHum, HIGH);
-  }
-  else{
-    digitalWrite(greenHum, LOW);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-  }
-  if (currHum < 30){
-    digitalWrite(yellowHum, HIGH);
-  }
-  else{
-    digitalWrite(yellowHum, LOW);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
-  }
-
-
-
-
-
-
-  Oled.setFont(u8x8_font_chroma48medium8_r); 
-  Oled.setCursor(0, 3);
-  Oled.print("Temp: ");
-  Oled.println(currTemp);
-  Oled.print("Humidity: ");
-  Oled.println(currHum);
+      Serial.println("Input minimum temperature (Celcius)");
+      min_temp = Serial.parseInt();
   
-  Serial.print("Temperature = ");
-  Serial.print(Environment.readTemperature()); //print temperature
-  Serial.println(" C");
-  Serial.print("Humidity = ");
-  Serial.print(Environment.readHumidity()); //print humidity
-  Serial.println(" %");
-  delay(1000);
+      Serial.println("Input maximum humidity (%)");
+      max_humid = Serial.parseInt();
+  
+      Serial.println("Input minimum humidity (%)");
+      min_humid = Serial.parseInt();
+  
+      curr_state = sGetData;
+      break;
+      
+    case sGetData:
+      curr_temp = Environment.readTemperature();
+      curr_humid = Environment.readHumidity();
+      Serial.print("Temperature = ");
+      Serial.print(curr_temp); //print temperature
+      Serial.println(" C");
+      Serial.print("Humidity = ");
+      Serial.print(curr_humid); //print humidity
+      Serial.println(" %");
+      delay(2000);
+      curr_state = sSetOutput;
+      break;
+      
+    case sSetOutput:
+      if (curr_temp > (float)max_temp){
+        digitalWrite(redTemp, HIGH);
+      }
+      else{
+        digitalWrite(redTemp, LOW);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+      }
+      if (curr_temp < (float)min_temp){
+        digitalWrite(blueTemp, HIGH);
+      }
+      else{
+        digitalWrite(blueTemp, LOW);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+      }
+    
+    
+      if (curr_humid > (float)max_humid){
+        digitalWrite(greenHum, HIGH);
+      }
+      else{
+        digitalWrite(greenHum, LOW);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+      }
+      if (curr_humid < (float)min_humid){
+        digitalWrite(yellowHum, HIGH);
+      }
+      else{
+        digitalWrite(yellowHum, LOW);                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                   
+      }
+
+
+      Oled.setFont(u8x8_font_chroma48medium8_r); 
+      Oled.setCursor(0, 3);
+      Oled.print("Temp: ");
+      Oled.println(curr_temp);
+      Oled.print("Humidity: ");
+      Oled.println(curr_humid);
+      
+      curr_state = sGetData;
+      break;
+      
+    default:
+      Serial.println("Why am I in default??");
+  }
+  
 }
